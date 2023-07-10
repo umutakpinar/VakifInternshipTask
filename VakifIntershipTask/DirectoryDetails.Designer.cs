541cff4131367ed9c7399e8ccc39a4f2c5d2cfce
Umut <57362066+umutakpinar@users.noreply.github.com>
2023-07-10 11:52:46 +0300

    Analizi dışarı aktar butonu eklendi.

--- VakifIntershipTask/DirectoryDetails.Designer.cs
+++ VakifIntershipTask/DirectoryDetails.Designer.cs
@@ -37,6 +37,7 @@
             this.lblNumberOfMissingContentFiles = new System.Windows.Forms.Label();
             this.dataGridView = new System.Windows.Forms.DataGridView();
             this.btnRefresh = new System.Windows.Forms.Button();
+            this.btnOutput = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
             this.SuspendLayout();
             // 
@@ -44,10 +45,9 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 16.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(17, 21);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(23, 26);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(315, 59);
+            this.label1.Size = new System.Drawing.Size(240, 45);
             this.label1.TabIndex = 0;
             this.label1.Text = "Directory Path :";
             // 
@@ -57,10 +57,9 @@
             this.lblPathDirectory.Cursor = System.Windows.Forms.Cursors.Hand;
             this.lblPathDirectory.Font = new System.Drawing.Font("Segoe UI", 16.125F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Italic | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblPathDirectory.ForeColor = System.Drawing.Color.MediumBlue;
-            this.lblPathDirectory.Location = new System.Drawing.Point(260, 21);
-            this.lblPathDirectory.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblPathDirectory.Location = new System.Drawing.Point(347, 26);
             this.lblPathDirectory.Name = "lblPathDirectory";
-            this.lblPathDirectory.Size = new System.Drawing.Size(697, 59);
+            this.lblPathDirectory.Size = new System.Drawing.Size(537, 45);
             this.lblPathDirectory.TabIndex = 1;
             this.lblPathDirectory.Text = "C:/Users/example/example/example";
             this.lblPathDirectory.Click += new System.EventHandler(this.lblPathDirectory_Click);
@@ -69,10 +68,9 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Segoe UI", 16.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(17, 91);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Location = new System.Drawing.Point(23, 114);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(742, 59);
+            this.label2.Size = new System.Drawing.Size(569, 45);
             this.label2.TabIndex = 2;
             this.label2.Text = "Number of DTO Files in this directory: ";
             // 
@@ -80,10 +78,9 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Segoe UI", 16.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(17, 156);
-            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Location = new System.Drawing.Point(23, 195);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(630, 59);
+            this.label3.Size = new System.Drawing.Size(485, 45);
             this.label3.TabIndex = 3;
             this.label3.Text = "DTO Files with missing Content :";
             // 
@@ -91,20 +88,18 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(17, 226);
-            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Location = new System.Drawing.Point(23, 282);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(0, 51);
+            this.label4.Size = new System.Drawing.Size(0, 38);
             this.label4.TabIndex = 4;
             // 
             // lblNumberOfDTOFiles
             // 
             this.lblNumberOfDTOFiles.AutoSize = true;
             this.lblNumberOfDTOFiles.Font = new System.Drawing.Font("Segoe UI", 16.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblNumberOfDTOFiles.Location = new System.Drawing.Point(744, 97);
-            this.lblNumberOfDTOFiles.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblNumberOfDTOFiles.Location = new System.Drawing.Point(992, 121);
             this.lblNumberOfDTOFiles.Name = "lblNumberOfDTOFiles";
-            this.lblNumberOfDTOFiles.Size = new System.Drawing.Size(48, 59);
+            this.lblNumberOfDTOFiles.Size = new System.Drawing.Size(38, 45);
             this.lblNumberOfDTOFiles.TabIndex = 5;
             this.lblNumberOfDTOFiles.Text = "0";
             // 
@@ -112,10 +107,9 @@
             // 
             this.lblNumberOfMissingContentFiles.AutoSize = true;
             this.lblNumberOfMissingContentFiles.Font = new System.Drawing.Font("Segoe UI", 16.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblNumberOfMissingContentFiles.Location = new System.Drawing.Point(639, 159);
-            this.lblNumberOfMissingContentFiles.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblNumberOfMissingContentFiles.Location = new System.Drawing.Point(852, 199);
             this.lblNumberOfMissingContentFiles.Name = "lblNumberOfMissingContentFiles";
-            this.lblNumberOfMissingContentFiles.Size = new System.Drawing.Size(48, 59);
+            this.lblNumberOfMissingContentFiles.Size = new System.Drawing.Size(38, 45);
             this.lblNumberOfMissingContentFiles.TabIndex = 6;
             this.lblNumberOfMissingContentFiles.Text = "0";
             // 
@@ -125,32 +119,46 @@
             this.dataGridView.AllowUserToDeleteRows = false;
             this.dataGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView.Location = new System.Drawing.Point(24, 226);
-            this.dataGridView.Margin = new System.Windows.Forms.Padding(2);
+            this.dataGridView.Location = new System.Drawing.Point(32, 282);
+            this.dataGridView.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.dataGridView.Name = "dataGridView";
             this.dataGridView.ReadOnly = true;
             this.dataGridView.RowHeadersWidth = 82;
             this.dataGridView.RowTemplate.Height = 33;
-            this.dataGridView.Size = new System.Drawing.Size(1132, 512);
+            this.dataGridView.Size = new System.Drawing.Size(1509, 640);
             this.dataGridView.TabIndex = 7;
             this.dataGridView.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView_CellClick);
             // 
             // btnRefresh
             // 
             this.btnRefresh.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnRefresh.Location = new System.Drawing.Point(1000, 43);
+            this.btnRefresh.Location = new System.Drawing.Point(1229, 57);
+            this.btnRefresh.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnRefresh.Name = "btnRefresh";
-            this.btnRefresh.Size = new System.Drawing.Size(156, 137);
+            this.btnRefresh.Size = new System.Drawing.Size(146, 171);
             this.btnRefresh.TabIndex = 8;
             this.btnRefresh.Text = "Refresh";
             this.btnRefresh.UseVisualStyleBackColor = true;
             this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
             // 
+            // btnOutput
+            // 
+            this.btnOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOutput.Location = new System.Drawing.Point(1395, 57);
+            this.btnOutput.Margin = new System.Windows.Forms.Padding(4);
+            this.btnOutput.Name = "btnOutput";
+            this.btnOutput.Size = new System.Drawing.Size(146, 171);
+            this.btnOutput.TabIndex = 9;
+            this.btnOutput.Text = "Create Output File";
+            this.btnOutput.UseVisualStyleBackColor = true;
+            this.btnOutput.Click += new System.EventHandler(this.btnOutput_Click);
+            // 
             // DirectoryDetails
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1184, 772);
+            this.ClientSize = new System.Drawing.Size(1579, 965);
+            this.Controls.Add(this.btnOutput);
             this.Controls.Add(this.btnRefresh);
             this.Controls.Add(this.dataGridView);
             this.Controls.Add(this.lblNumberOfMissingContentFiles);
@@ -160,7 +168,7 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.lblPathDirectory);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "DirectoryDetails";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Directory Details";
@@ -183,5 +191,6 @@
         private System.Windows.Forms.Label lblNumberOfMissingContentFiles;
         private System.Windows.Forms.DataGridView dataGridView;
         private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.Button btnOutput;
     }
 }
--- VakifIntershipTask/DirectoryDetails.cs
+++ VakifIntershipTask/DirectoryDetails.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace VakifIntershipTask
@@ -65,6 +66,52 @@ namespace VakifIntershipTask
             TaskManager.LoadData(lblPathDirectory, _selectedPath, _files, _fileInfos, _fileInfosHasTheMissingContent, lblNumberOfDTOFiles, lblNumberOfMissingContentFiles, dataGridView);
             dataGridView.Update();
             dataGridView.Refresh();
+        }
+
+        private void btnOutput_Click(object sender, EventArgs e)
+        {
+            if (Convert.ToInt32(lblNumberOfMissingContentFiles.Text) != 0)
+            {
+                List<string> headers = new List<string>();
+                SaveFileDialog saveFileDialog = new SaveFileDialog();
+                int indexOfLastSlash = _selectedPath.LastIndexOf("/");
+                //string saveFilePath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop); ; //Belki burada varsayılan olarak desktop seçili hale getirilebilir.
+                saveFileDialog.FileName = _selectedPath.Substring(indexOfLastSlash+1) + "_analysis";
+                saveFileDialog.Filter = "Text Files (*.txt)|*.txt";
+                if(saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string fileName = saveFileDialog.FileName;
+                    //string filePath = Path.GetDirectoryName(fileName);
+                    using (StreamWriter writer = new StreamWriter(fileName))
+                    {
+                        foreach (DataGridViewColumn column in dataGridView.Columns)
+                        {
+                            headers.Add(column.HeaderText);
+                        }
+                        writer.WriteLine(DateTime.Now);
+                        writer.WriteLine("Directory Path : {0}", _selectedPath);
+                        writer.WriteLine("Number of DTO Files With Missing Content : {0}",lblNumberOfMissingContentFiles.Text);
+                        writer.Write("\n*****************************************");
+                        foreach (DataGridViewRow row in dataGridView.Rows)
+                        {
+                            writer.Write("\n");
+                            foreach (DataGridViewCell cell in row.Cells)
+                            {
+                                writer.Write("{0} : ", headers[cell.ColumnIndex]);
+                                writer.Write(cell.Value.ToString());
+                                writer.Write("\n");
+                            }
+                            writer.Write("\n");
+                            writer.Write("*****************************************");
+                        }
+                    }
+                    MessageBox.Show("Output file {0} created.", fileName);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Everything looks fine for this directory. Nothing to dump.");
+            }
         }
     }
 }
